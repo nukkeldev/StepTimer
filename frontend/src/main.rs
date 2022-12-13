9d716154b76ca4e71ed316c170c4a854ee4c909a
nukkeldev <nukkel.dev@gmail.com>
2022-12-12 20:54:41 -0800

    Rewrote so it actually works

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,25 +1,23 @@
 use std::rc::Rc;
 
-use gloo_timers::{callback::{Interval, Timeout}, future::{IntervalStream, TimeoutFuture}};
+use gloo_timers::{
+    callback::{Interval, Timeout},
+    future::{IntervalStream, TimeoutFuture},
+};
 use yew::prelude::*;
+
+pub mod stopwatch;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use web_sys::{console, window};
 
+use crate::stopwatch::Stopwatch;
+
 #[wasm_bindgen(module = "/public/glue.js")]
 extern "C" {
     #[wasm_bindgen(js_name = invokeHello, catch)]
     pub async fn hello(name: String) -> Result<JsValue, JsValue>;
-}
-
-#[macro_use]
-macro_rules! dial {
-    ($progress:expr,$end:expr,$thickness:expr) => {
-        html! {
-            <Dial progress={50}
-        }
-    };
 }
 
 #[function_component]
@@ -37,100 +35,9 @@ fn App() -> Html {
     html! {
         <ContextProvider<Rc<Theme>> context={theme}>
             <div class="dial-wrapper">
-                <Dial progress={50} end={100} thickness={10} tick_time={1000} />
+                <Stopwatch start={0} progress={50} end={100} />
             </div>
         </ContextProvider<Rc<Theme>>>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-pub struct DialProps {
-    progress: i32,
-    end: i32,
-    tick_time: u32,
-    thickness: u8,
-}
-
-enum DialAction {
-    Tick,
-    Pause,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-struct DialState {
-    progress: i32,
-    end: i32,
-    tick_time: u32,
-}
-
-impl Default for DialState {
-    fn default() -> Self {
-        Self {
-            progress: 0,
-            end: 100,
-            tick_time: 500,
-        }
-    }
-}
-
-impl Reducible for DialState {
-    type Action = DialAction;
-
-    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
-        let next = match action {
-            DialAction::Tick => self.progress + 1,
-            DialAction::Pause => self.progress,
-        };
-
-        Self {
-            progress: next,
-            end: self.end,
-            tick_time: self.tick_time,
-        }
-        .into()
-    }
-}
-
-#[function_component]
-pub fn Dial(props: &DialProps) -> Html {
-    let DialProps {
-        progress,
-        end,
-        thickness,
-        tick_time
-    } = props;
-
-    let theme = use_context::<Rc<Theme>>().unwrap();
-
-    let dial = use_reducer(|| DialState {
-        progress: progress.clone(),
-        end: end.clone(),
-        tick_time: tick_time.clone(),
-    });
-
-    let dial_clone = dial.clone();
-
-    use_state(|| spawn_local(async move {
-        let dial = dial_clone.clone();
-
-        let interval = Interval::new(dial.tick_time, move || {
-            let dial = dial.clone();
-            dial.dispatch(DialAction::Tick);
-        });
-
-        let dial = dial_clone.clone();
-
-        TimeoutFuture::new((dial.end - dial.progress + 1) as u32 * dial.tick_time).await;
-
-        interval.cancel();
-    }));
-
-    html! {
-        <div class="dial"
-            style={format!("--p:{};--b:{}px;--c:{};", dial.progress, thickness, theme.primary)} // --c: Color --p: Percent --b: Border Thickness
-        >
-            {format!("{}/{}", dial.progress, end)}
-        </div>
     }
 }
 
--- frontend/src/stopwatch.rs
+++ frontend/src/stopwatch.rs
@@ -1,19 +1,97 @@
-pub struct Stopwatch;
+use std::rc::Rc;
+
+use gloo_timers::callback::Interval;
+use yew::{html, use_context, Callback, Component, Context, Html, Properties};
+
+use crate::Theme;
+
+pub struct Stopwatch {
+    start: i32,
+    progress: i32,
+    end: i32,
+
+    interval: Option<Interval>,
+}
+
+#[derive(PartialEq, Properties, Clone)]
+pub struct StopwatchProps {
+    pub start: i32,
+    pub progress: i32,
+    pub end: i32,
+}
 
 pub enum StopwatchMsg {
+    Start,
+    Pause,
+    Reset,
+    Tick,
 }
 
 impl Component for Stopwatch {
     type Message = StopwatchMsg;
-    type Properties = ();
+    type Properties = StopwatchProps;
 
     fn create(ctx: &Context<Self>) -> Self {
-        Self
+        let Self::Properties {
+            start,
+            progress,
+            end,
+        } = ctx.props();
+        Self {
+            start: start.clone(),
+            progress: progress.clone(),
+            end: end.clone(),
+            interval: None,
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            StopwatchMsg::Start => {
+                let ticker = {
+                    let link = ctx.link().clone();
+                    Interval::new(1000, move || {
+                        link.send_message(StopwatchMsg::Tick);
+                    })
+                };
+                self.interval = Some(ticker);
+            }
+            StopwatchMsg::Pause => {
+                self.interval.take();
+            }
+            StopwatchMsg::Reset => {
+                self.progress = self.start;
+            }
+            StopwatchMsg::Tick => {
+                self.progress += 1;
+            }
+        }
+
+        true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let (theme, _) = ctx
+            .link()
+            .context::<Rc<Theme>>(Callback::from(|_| {}))
+            .unwrap();
+
+        let is_paused = self.interval.is_none();
+
+        let onclick = ctx.link().callback(move |_| {
+            if is_paused {
+                StopwatchMsg::Start
+            } else {
+                StopwatchMsg::Pause
+            }
+        });
+
         html! {
-            
+            <div class="dial"
+                style={format!("--p:{};--b:{}px;--c:{};", self.progress, 10, theme.primary)} // --c: Color --p: Percent --b: Border Thickness
+            >
+                <button {onclick}>{format!("{}/{}", self.progress, self.end)}</button>
+            </div>
         }
     }
 }
